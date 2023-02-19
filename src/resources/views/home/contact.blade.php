0154fa3e6e1fe51fc582589af3e7aa429492cf82
drew <58788781+drew138@users.noreply.github.com>
2023-02-18 19:40:35 -0500

    entrega 1

---
+++ src/resources/views/home/contact.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+
+@section('content')
+<div class="text-center">
+  <h2>Phone<h2/>
+  300 12 34
+</div>
+
+<div class="text-center">
+  <h2>Email<h2/>
+  test@email.com
+</div>
+
+<div class="text-center">
+  <h2>Addresss<h2/>
+  Street 1
+</div>
+
+@endsection
