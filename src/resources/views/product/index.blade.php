a44e9327f48f6881203e19a62e1202cf6855bfe4
drew <58788781+drew138@users.noreply.github.com>
2023-03-04 14:06:00 -0500

    entrega 3

--- src/resources/views/product/index.blade.php
+++ src/resources/views/product/index.blade.php
@@ -1,34 +1,28 @@
 @extends('layouts.app')
-
-@section('title', $viewData["title"])
-
+@section('title', $data["title"])
 @section('subtitle', $viewData["subtitle"])
-
 @section('content')
-
 <div class="row">
-
-    @foreach ($viewData["products"] as $product)
-
+    @foreach ($data["orders"] as $order)
     <div class="col-md-4 col-lg-3 mb-2">
-
         <div class="card">
-
             <img src="https://laravel.com/img/logotype.min.svg" class="card-img-top img-card">
-
             <div class="card-body text-center">
-
                 <a href="{{ route('product.show', ['id'=> $product['id']]) }}" class="btn bg-primary text-white">{{ $product["name"] }}</a>
-
             </div>
-            <p>Price: {{ $product["price"]}}
+            <p>{{ __('orders.total') }}: {{ $order->getTotal()}}
                 <p />
+            <p>{{ __('orders.is_') }}: {{ $order->getTotal()}}
+                <p />
+            <form action="{{ route('order.destroy', ['id'=> $order->getId()]) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-danger">
+                    {{ __('reviews.delete') }}
+                </button>
+            </form>
         </div>
-
     </div>
-
     @endforeach
-
 </div>
-
 @endsection
--- src/routes/web.php
+++ src/routes/web.php
@@ -19,34 +19,51 @@ Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index")
 
 Route::get('/about', function () {
 
-$data1 = "About us - Online Store";
+    $data1 = "About us - Online Store";
 
-$data2 = "About us";
+    $data2 = "About us";
 
-$description = "This is an about page ...";
+    $description = "This is an about page ...";
 
-$author = "Developed by: Andres Salazar";
+    $author = "Developed by: Andres Salazar";
 
-return view('home.about')->with("title", $data1)
+    return view('home.about')->with("title", $data1)
 
-->with("subtitle", $data2)
+        ->with("subtitle", $data2)
 
-->with("description", $description)
+        ->with("description", $description)
 
-->with("author", $author);
-
+        ->with("author", $author);
 })->name("home.about");
 
 
 
-Route::get('/contact', function () { return view('home.contact');})-> name("home.contact");
+Route::get('/contact', function () {
+    return view('home.contact');
+})->name("home.contact");
 
 Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("product.index");
 
 Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name("product.create");
 
-Route::get('/products/created', function () {return view('product.created');})->name("product.created");
+Route::get('/products/created', function () {
+    return view('product.created');
+})->name("product.created");
 
 Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name("product.save");
 
 Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show");
+
+Route::get('/cart', 'App\Http\Controllers\CartController@index')->name("cart.index");
+
+Route::get('/cart/add/{id}', 'App\Http\Controllers\CartController@add')->name("cart.add");
+
+Route::get('/cart/removeAll/', 'App\Http\Controllers\CartController@removeAll')->name("cart.removeAll");
+
+Route::get('/image', 'App\Http\Controllers\ImageController@index')->name("image.index");
+
+Route::post('/image/save', 'App\Http\Controllers\ImageController@save')->name("image.save");
+
+Route::get('/image-not-di', 'App\Http\Controllers\ImageNotDIController@index')->name("imagenotdi.index");
+
+Route::post('/image-not-di/save', 'App\Http\Controllers\ImageNotDIController@save')->name("imagenotdi.save");
